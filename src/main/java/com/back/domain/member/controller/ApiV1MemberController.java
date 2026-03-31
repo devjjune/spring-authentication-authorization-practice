b0498de6dc97c6049d40f707a0d948130ba1bd81
devjjune <jiyoon.hwang92@gmail.com>
2026-03-31 14:31:36 +0900

    17강. 로그인 기능 구현 (TDD)

--- src/main/java/com/back/domain/member/controller/ApiV1MemberController.java
+++ src/main/java/com/back/domain/member/controller/ApiV1MemberController.java
@@ -3,6 +3,7 @@ package com.back.domain.member.controller;
 import com.back.domain.member.dto.MemberDto;
 import com.back.domain.member.entity.Member;
 import com.back.domain.member.service.MemberService;
+import com.back.global.exception.ServiceException;
 import com.back.global.rsData.RsData;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiV1MemberController {
     private final MemberService memberService;
 
+    // 회원가입 요청 시 클라이언트가 보내는 JSON 데이터를 담는 DTO
     record MemberJoinReqBody(
             String username,
             String password,
@@ -24,12 +26,13 @@ public class ApiV1MemberController {
     ) {
     }
 
+    // 회원가입 처리 후 생성된 회원 정보를 응답으로 전달하기 위한 DTO
     record MemberJoinResBody(
             MemberDto memberDto
     ) {
     }
 
-    @PostMapping
+    @PostMapping("/join")
     public RsData<MemberDto> join(@RequestBody @Valid MemberJoinReqBody reqBody) {
 
         Member member = memberService.join(reqBody.username, reqBody.password, reqBody.nickname);
@@ -40,6 +43,37 @@ public class ApiV1MemberController {
                 new MemberJoinResBody(
                         new MemberDto(member)
                 )
+        );
+    }
+
+    // 로그인 요청 시 클라이언트가 보내는 인증 정보(username, password)를 담는 DTO
+    record MemberLoginReqBody(
+            String username,
+            String password
+    ) {
+    }
+
+    // 로그인 성공 시 발급된 인증 정보(apiKey)를 응답으로 전달하기 위한 DTO
+    record MemberLoginResBody(
+            String apiKey
+    ) {
+    }
+
+    @PostMapping("/login")
+    public RsData<MemberLoginResBody> login(@RequestBody @Valid MemberLoginReqBody reqBody) {
+
+        Member actor = memberService.findByUsername(reqBody.username).orElseThrow(
+                () -> new ServiceException("401-1", "존재하지 않는 아이디입니다.")
+        );
+
+        if(!actor.getPassword().equals(reqBody.password)){
+            throw new ServiceException("401-2", "비밀번호가 일치하지 않습니다.");
+        }
+
+        return new RsData(
+                "%s님 환영합니다.".formatted(actor.getName()),
+                "200-1",
+                new MemberLoginResBody(actor.getApiKey())
         );
     }
 }
